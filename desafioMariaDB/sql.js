3ba0f196226130c30968772e61d933053de2c6c6
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2022-11-30 22:27:03 -0300

    7 ma entrega

---
+++ desafioMariaDB/sql.js
@@ -0,0 +1,42 @@
+import knexLib from 'knex';
+
+class ClientSql {
+  constructor(config) {
+    this.knex = knexLib(config);
+  }
+
+  createTable() {
+    return this.knex.schema.dropTableIfExists('products')
+      .finally(() => {
+        return this.knex.schema.createTable('products', table => {
+          table.increments('id').primary();
+          table.string('nombre', 50).notNullable();
+          table.string('codigo', 10).notNullable();
+          table.float('precio');
+          table.integer('stock');
+        })
+      })
+  }
+
+  insertProducts(products) {
+    return this.knex('products').insert(products);
+  }
+
+  listProducts() {
+    return this.knex('products').select('*');
+  }
+
+  deleteProductById(id) {
+    return this.knex.from('products').where('id', id).del();
+  }
+
+  updateStockById(stock, id) {
+    return this.knex.from('products').where('id', id).update({ stock: stock });
+  }
+
+  close() {
+    this.knex.destroy();
+  }
+}
+
+export default ClientSql;
